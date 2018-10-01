4dde86a99ea0a2ed832eaa6cd2bb8af50d8610a6
Mottaqui Karim <mottaquikarim@users.noreply.github.com>
2018-10-01 00:31:29 -0400

    Create pset1.js

---
+++ pset1.js
@@ -0,0 +1,9 @@
+/* 1
+    @func arrSwap
+    @param {array} arr
+    @returns {array}
+    @desc - given an array, swap the last and first items
+            using array methods ONLY (not by index)
+            otherwise return string
+    @example - arrSwap([1,2,3,4]);  // [4,2,3,1]
+*/
