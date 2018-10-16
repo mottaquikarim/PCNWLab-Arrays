0d3e7714bf315981a1100405973c594a351b5b65
Mohammed Mosayed <mohammedmosayed@gmail.com>
2018-10-16 18:00:32 -0400

    psets1 & 2

--- pset1.js
+++ pset1.js
@@ -1,4 +1,24 @@
 /* 1
+    @func longestArr
+    @param {array} arr1
+    @param {arrar} arr2
+    @returns {array}
+    @desc - given two arrays return the longest array
+    @example - longestArr([1,2,3,4], ['dog', 'cat']);  // [1, 2, 3, 4]
+*/
+
+/* 2
+    @func arrSwapIndex
+    @param {array} arr
+    @returns {array}
+    @desc - given an array, swap the last and first items
+            using INDEX only (not using array methods)
+            otherwise return string error
+    @example - arrSwapIndex([1,2,3,4]);  // [4,2,3,1]
+*/
+
+
+/* 3
     @func arrSwap
     @param {array} arr
     @returns {array}
@@ -7,3 +27,52 @@
             otherwise return string
     @example - arrSwap([1,2,3,4]);  // [4,2,3,1]
 */
+
+
+/* 4
+    @func combineArr
+    @param {array} arr1
+    @param {arrar} arr2
+    @returns {array}
+    @desc - given two arrays return ONE array with
+            all the items from both arrays 
+    @example - combineArr([1,2,3,4], ['dog', 'cat']);  // [1, 2, 3, 4, 'dog', 'cat']
+*/
+
+
+/* 5
+    @func getRandomItem
+    @param {array} arr1
+    @returns {object} 
+    @desc - given an array, return one random
+            item from the array.
+    @example - getRandomItem([1,2,3,4,5,6,7,8]);  // 7
+*/
+
+
+/* 6
+    @func getMiddleItem
+    @param {array} arr
+    @returns {object} 
+    @desc - given an array, return the middle element.
+            If the array is even in length, return the 
+            first "middle item"
+    @example - getMiddleItem([1,2,3,4,5,6,7]);  // 4
+    @example - getMiddleItem([1,2,3,4,5,6,7,8]);  // 4
+*/
+
+
+
+/* 7
+    @func getMedianItem
+    @param {array} arr
+    @returns {object} 
+    @desc - given an array, return the middle element.
+            If the array is even in length, return the 
+            average of the middle two.
+    @example - getMedianItem([1,2,3,4,5,6,7]);  // 4
+    @example - getMedianItem([1,2,3,4,5,6,7,8]);  // 4.5
+*/
+
+
+
---
+++ pset2.js
@@ -0,0 +1,73 @@
+/* 1
+    @func getRange
+    @param {array} arr1
+    @param {number} min
+    @param {number} max
+    @returns {array}
+    @desc - given an array and a range, return all the 
+            elements in the array that fall within the range.
+            The min and max is inclusive. 
+            Min and Max is represented by the element number,
+            not the index number.
+
+    @example - getRange([1,2,3,4,5], 1, 3]);  // [1, 2, 3]
+    @example - getRange([1,2,3,4,5], 3, 5]);  // [3, 4, 5]
+    @example - getRange([1,2,3,4,5], 5, 5]);  // Error: Out of Range
+    @example - getRange([1,2,3,4,5], 3, 100]);  // Error: Out of Range
+    @example - getRange([1,2,3,4,5], 6, 1]);  // Error: Out of Range
+
+*/
+
+
+
+
+/* 2
+    @func insertIntoMiddle
+    @param {array} arr1
+    @param {array} arr2
+    @returns {array}
+    @desc - given two arrays, return a combined array, 
+            with the second array inserted into the middle  
+            of the first array.
+
+    @example - insertIntoMiddle([1,2,3,4,5], ['cat', 'dog']);  // [1, 2, 3, 'cat', 'dog, 4, 5]
+    @example - insertIntoMiddle([1,2,3,4,5,6], ['cat', 'dog']);  // [1, 2, 3, 'cat', 'dog, 4, 5, 6]
+*/
+
+
+
+
+/* 3
+    @func reverseArr
+    @param {array} arr
+    @returns {array}
+    @desc - given an array of numbers, return back the contents of the 
+            array in reverse order. 
+            !Important: You may not use the arr.reverse() method.
+            !Important: You may not use any form of loops
+
+    @example - reverseArr([1,2,3,4,5]);  // [5,4,3,2,1]
+*/
+
+
+
+
+/* 4
+    @func reversedRangeMasher
+    @param {array} arr1
+    @param {array} arr2
+    @param {array} min
+    @param {array} max
+
+    @returns {array}
+    @desc - given two arrays, take the second array and reverse the elements. 
+            Get the range of the first array, and insert the reversed array  
+            into the middle of the range of the first array.
+
+    @example - reversedRangeMasher([1,2,3,4,5], ['cat', 'dog'], 3, 5);  // [3, 4, 'god', 'tac', 5]
+    @example - reversedRangeMasher([1,2,3,4,5], ['hello', 'world'], 1, 5);  // [1, 2, 3, 'dlrow', 'olleh', 4, 5]
+
+*/
+
+
+
