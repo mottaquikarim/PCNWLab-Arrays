b45b1e81db1c52a732aef206f153bc6f13b7c3a2
Syed Aziz <syedaziz27x@gmail.com>
2018-10-16 22:10:48 -0400

    did p6

--- pset1.js
+++ pset1.js
@@ -98,7 +98,7 @@ const getMiddleItem = arr => {
     return arr[Math.floor((arr.length/2)) ] 
  
     }
-let arrrr = [1,2,3,4,5,6,7];
+let arrrr = [1,2,3,4,5,6,7,8,9];
 console.log(getMiddleItem(arrrr));
 
 /* 7
