b386df7d77abc82d5750786dfd856e948f0d7a19
Alexander <aionate0812@gmail.com>
2018-10-17 19:32:22 -0400

    answers to pset2

--- pset2.js
+++ pset2.js
@@ -17,7 +17,21 @@
     @example - getRange([1,2,3,4,5], 6, 1]);  // Error: Out of Range
 
 */
+const getRange = (arr, min,max) => {
+   // console.log('gerRange runs')
+    if (arr.lastIndexOf(min)>=0 && arr.lastIndexOf(max)>=0 && (arr.lastIndexOf(max)-arr.lastIndexOf(min)) === (max-min) && max>min ){
+      // console.log('if works')
+        return arr.slice(arr[arr.lastIndexOf(min)-1],arr[arr.lastIndexOf(max)])
+    }
+    return "Error: Out of Range"
+        
+}
 
+console.log(getRange([1,2,3,4,5], 1, 3))
+console.log(getRange([1,2,3,4,5], 3, 5))
+console.log(getRange([1,2,3,4,5], 5, 5))
+console.log(getRange([1,2,3,4,5], 3, 100))
+console.log(getRange([1,2,3,4,5], 6, 1))
 
 
 
@@ -33,9 +47,12 @@
     @example - insertIntoMiddle([1,2,3,4,5], ['cat', 'dog']);  // [1, 2, 3, 'cat', 'dog, 4, 5]
     @example - insertIntoMiddle([1,2,3,4,5,6], ['cat', 'dog']);  // [1, 2, 3, 'cat', 'dog, 4, 5, 6]
 */
+const insertIntoMiddle = (arr1,arr2) => {
+    return arr1.slice(0,Math.ceil(arr1.length/2)).concat(arr2).concat(arr1.slice(Math.ceil(arr1.length/2)))
+}
 
-
-
+console.log(insertIntoMiddle([1,2,3,4,5], ['cat', 'dog']))
+console.log(insertIntoMiddle([1,2,3,4,5,6], ['cat', 'dog']))
 
 /* 3
     @func reverseArr
@@ -48,7 +65,11 @@
 
     @example - reverseArr([1,2,3,4,5]);  // [5,4,3,2,1]
 */
+const reverseArr = (arr) => {
+    return arr.sort((a, b)=>{return b-a});
+}
 
+console.log(reverseArr([1,2,3,4,5]))
 
 
 
@@ -68,6 +89,22 @@
     @example - reversedRangeMasher([1,2,3,4,5], ['hello', 'world'], 1, 5);  // [1, 2, 3, 'dlrow', 'olleh', 4, 5]
 
 */
+const reverseString = (str) => {
+    const strArr = str.split('')
+    const reverseString = strArr.reverse().join('')
+    return reverseString
+}
 
 
+const reversedRangeMasher = (arr1,arr2, min,max) => {
+    let reverseArr2 = []
 
+for (let i = 0; i<arr2.length; i++){
+reverseArr2.push(reverseString(arr2[i])) 
+}
+
+return insertIntoMiddle(getRange(arr1,min,max),reverseArr2.reverse()) 
+
+}
+console.log(reversedRangeMasher([1,2,3,4,5], ['cat', 'dog'], 3, 5))
+console.log(reversedRangeMasher([1,2,3,4,5],['hello', 'world'], 1, 5))
