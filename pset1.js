e9e0d8657ccbf732f81d9630bac74f1a158665a4
Mielyn Acosta <mielynacosta@pursuit.org>
2018-10-16 21:49:38 -0400

    pset1 problems solved

--- pset1.js
+++ pset1.js
@@ -6,7 +6,16 @@
     @desc - given two arrays return the longest array
     @example - longestArr([1,2,3,4], ['dog', 'cat']);  // [1, 2, 3, 4]
 */
+ const longestArr = (arr1, arr2) => {
+    const array1 = arr1.length;
+    const array2 = arr2.length 
+    if (array1 > array2) {
+           return arr1; 
 
+    }   return arr2 
+ } 
+
+ console.log(longestArr([1,2,3,4], ['dog','cat']));
 /* 2
     @func arrSwapIndex
     @param {array} arr
@@ -16,7 +25,17 @@
             otherwise return string error
     @example - arrSwapIndex([1,2,3,4]);  // [4,2,3,1]
 */
+const arrSwapIndex = arr => {
+   const firstItem = arr[0]; 
+   const lastItem = arr[arr.length -1]; 
 
+   arr[0] = lastItem; 
+   arr[arr.length -1] = firstItem;
+
+   return arr; 
+}
+
+console.log(arrSwapIndex([1,2,3,4])); 
 
 /* 3
     @func arrSwap
@@ -27,8 +46,19 @@
             otherwise return string
     @example - arrSwap([1,2,3,4]);  // [4,2,3,1]
 */
+ const arrSwap = arr => {
+     const firstElement = arr[0]; 
+     const lastElement =  arr.length - 1; 
+ 
+    arr.shift(); 
+    arr.unshift(lastElement);
+    arr.pop();
+    arr.push(firstElement); 
+    
+    return arr; 
 
-
+ }
+ console.log(arrSwap([1,2,3,4]));
 /* 4
     @func combineArr
     @param {array} arr1
@@ -38,8 +68,8 @@
             all the items from both arrays 
     @example - combineArr([1,2,3,4], ['dog', 'cat']);  // [1, 2, 3, 4, 'dog', 'cat']
 */
-
-
+combineArr = (arr1,arr2) => arr1.concat(arr2); 
+console.log(combineArr([1,2,3,4], ['dog', 'cat']));
 /* 5
     @func getRandomItem
     @param {array} arr1
@@ -48,8 +78,12 @@
             item from the array.
     @example - getRandomItem([1,2,3,4,5,6,7,8]);  // 7
 */
+const getRandomItem = arr1 => {
+   return arr1[Math.floor(Math.random() * arr1.length)]; 
+     
 
-
+}
+console.log(getRandomItem([1,2,3,4,5,6,7,8]));
 /* 6
     @func getMiddleItem
     @param {array} arr
@@ -61,8 +95,14 @@
     @example - getMiddleItem([1,2,3,4,5,6,7,8]);  // 4
 */
 
+const getMiddleItem = arr => {
+       const middleIndex = Math.floor(arr.length / 2); 
+       
+       return arr[middleIndex];
 
-
+}
+console.log(getMiddleItem([1,2,3,4,5,6,7]));
+console.log(getMiddleItem([1,2,3,4,5,6,7]));
 /* 7
     @func getMedianItem
     @param {array} arr
